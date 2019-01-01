2ea1e8a8588a35b7a53ffb4ba6669c2f1fd91c36
dsaltares <dsaltares@bloomberg.net>
2019-01-01 17:16:33 +0000

    setup enzyme testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+test('renders a header', () => {
+    const app = shallow(<App />);
+    expect(app.is('div')).toEqual(true);
 });
